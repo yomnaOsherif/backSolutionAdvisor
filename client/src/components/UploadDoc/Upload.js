8166df64b130aeb0ae610b30cb6122955ca6e6e5
Yomna Sherif <yomna.sherif@ibm.com  git config --global user.name yomnaOsherifgit config --global user.email yomna.sherif@ibm.com>
2021-06-28 15:01:39 +0200

    recommendation shows in upload doc page

--- client/src/components/UploadDoc/Upload.js
+++ client/src/components/UploadDoc/Upload.js
@@ -2,57 +2,67 @@ import React from 'react'
 import { Button } from "react-bootstrap";
 import Header from "../header/header";
 //import Discovery from "../discovery/discovery";
-import axios from "../../axiosInstance";
+import axios  from 'axios';
+import { environment } from "../../Environments/environment";
+import { toast } from "react-toastify";
 
 class Upload extends React.Component {
   constructor() {
   super();
   //const [selectedFile, setSelectedFile] = this.state();
   //const [ isSelected ,setIsSelected] = this.state(false);
-  this.state = {
-    Recs: []
-   
-  };
-}
 
-/*selectedFile:[],
-setSelectedFile:[],
-isSelected:[],
-setIsSelected:[]*/
+  this.state = {
+    solution: "",
+    confidence: "",
+    selectedFile:[],
+  setSelectedFile:[],
+  isSelected: false,
+  setIsSelected: false
+ };
+
+}
+onFileChange = event => {
+    
+  // Update the state
+  this.setState({ selectedFile: event.target.files[0] });
+
+};
+onFileUpload = () => { 
+  // Create an object of formData 
+  const formData = new FormData(); 
+  // Update the formData object 
+  formData.append( 
+    "myFile", 
+    this.state.selectedFile, 
+    this.state.selectedFile.name,
+    this.state.selectedFile.type,
+    this.state.selectedFile.size
+  ); 
+  axios.post(environment.host + '/discoveryy/doc', {
+    formData
+  }).then(res => {
+    console.log("uploaded successfully");
+    toast.success('Document Uploaded Successfully')
+  })
+  }
 componentDidMount() {
-  this.getRecommendation();
+  //this.getRecommendation();
+  //this.fileData();
  
 }
+
+
 
 getRecommendation() {
-  axios.get("http://192.168.1.102:5000/api/discoveryy").then(res => {
-    this.setState({ Recs: res.data.data });
-  });
-}
-refreshAlbums() {
-  axios.get("http://localhost:5000/api/discoveryy").then(res => {
-    this.setState({ Recs: res.data.data });
+  axios.get(environment.host + '/discoveryy/rec').then(res => {
+    this.setState({ solution: res.data.data.result.results[0].answer, confidence: res.data.data.result.results[0].result_metadata.confidence });
+
   });
 }
 
- 
 
-	 /*changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsSelected(true);
-    }
-     hiddenFileInput = React.useRef(null);
-  
-     handleClick = event => {
-      hiddenFileInput.current.click();
-    };
-     handleSubmission = () => {
-		const formData = new FormData();
-		formData.append('File', selectedFile);
 
-			
-	};*/
-    
    render(){
     return(
         
@@ -63,14 +73,28 @@ refreshAlbums() {
 </div>
 <div className="col-8 align-self-center">
        
+<div> 
+                <input type="file" onChange={this.onFileChange} /> 
+                <Button onClick={this.onFileUpload}> 
+                  Upload File 
+                </Button> 
+            </div> 
+          Additional Information <br></br>
+   File type: {this.state.selectedFile.type} <br></br>
+  File Size in bytes: {this.state.selectedFile.size}
+
 
                  <div>
-                 <Button onClick={this.getRecommendation}>
+                 <Button onClick={this.getRecommendation.bind(this)}>
       Get Recommendation
       </Button>
              </div>
+             <div>Solution Recommended {this.state.solution} <br></br>
+             Confidence Score {this.state.confidence}
+         </div>
              </div>
              </div>
+           
 
 
          )
--- pages/discoveryy.js
+++ pages/discoveryy.js
@@ -2,36 +2,23 @@ const express = require("express");
 const router = express.Router();
 const DiscoveryV1 = require('ibm-watson/discovery/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
+const File = require("../models/Docs");
 
-
-
-router.get("/", async (req, res) => {
 
 const discovery = new DiscoveryV1({
-    version: '2019-04-30',
-    authenticator: new IamAuthenticator({
-      apikey: 'zxIfLbDWvLj6bGTg7v7wr4pJ1RGpJlfyge4T-mSLdTRq',
-    }),
-  serviceUrl: 'https://api.eu-de.discovery.watson.cloud.ibm.com/instances/f014925c-f2b8-4b02-ab32-3efcc1cd427d',
-  disableSslVerification: true
+  version: '2019-04-30',
+  authenticator: new IamAuthenticator({
+    apikey: 'zxIfLbDWvLj6bGTg7v7wr4pJ1RGpJlfyge4T-mSLdTRq',
+  }),
+serviceUrl: 'https://api.eu-de.discovery.watson.cloud.ibm.com/instances/f014925c-f2b8-4b02-ab32-3efcc1cd427d',
+disableSslVerification: true
 
 });
 
-const addDocumentParams = {
-    environmentId: 'e6d028cf-884a-4004-9131-7bae292ed0d9',
-    collectionId: 'd2c0c8dc-4941-4667-855c-7f28f0baaf56'
-   // file: fs.createReadStream('./sampleWord.docx'),
-  };
-  
-  /*discovery.addDocument(addDocumentParams)
-    .then(documentAccepted => {
-      const documentAccepted = response.result;
-      console.log(JSON.stringify(documentAccepted, null, 2));
-    })
-    .catch(err => {
-      console.log('error:', err);
-    });
-*/
+router.get("/rec", async (req, res) => {
+
+
+
     const queryParams = {
         environmentId: 'e6d028cf-884a-4004-9131-7bae292ed0d9',
         collectionId: 'd2c0c8dc-4941-4667-855c-7f28f0baaf56',
@@ -45,6 +32,8 @@ const addDocumentParams = {
       discovery.query(queryParams)
         .then(queryResponse => {
           console.log(JSON.stringify(queryResponse, null,2));
+          res.json({ data: queryResponse })
+
         })
         .catch(err => {
           console.log('error:', err);
@@ -52,4 +41,26 @@ const addDocumentParams = {
 
     })
 
+    router.post("/doc", async (req, res) => {
+      const file = req.body
+    const newFile = new File({
+        file
+    });
+
+    const addDocumentParams = {
+      environmentId: 'e6d028cf-884a-4004-9131-7bae292ed0d9',
+      collectionId: 'd2c0c8dc-4941-4667-855c-7f28f0baaf56',
+      file: newFile
+    };
+    
+  discovery.addDocument(addDocumentParams)
+  .then(documentAccepted => {
+    console.log(JSON.stringify(documentAccepted, null, 2));
+    res.json({ data: documentAccepted })
+
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
+})
     module.exports = router;
